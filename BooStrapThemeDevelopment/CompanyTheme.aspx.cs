d5519f2eb8f0950ef0a9234fbef10669dc015908
MahbubRiyad <mahbubriad88@gmail.com>
2015-11-11 23:14:50 +0600

    three web template

---
+++ BooStrapThemeDevelopment/CompanyTheme.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BooStrapThemeDevelopment
+{
+    public partial class CompanyTheme : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BooStrapThemeDevelopment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BooStrapThemeDevelopment.Global" Language="C#" %>
